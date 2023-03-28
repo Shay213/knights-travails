2f9056742dd96030e3b97e4f59cf538b345f47c4
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-03-28 15:36:40 +0200

    Include warnsdorff rule in dfs

--- main.js
+++ main.js
@@ -1,40 +1,63 @@
-const knightTour = start => {
-    const board = Array.from({length: 8}, () => Array.from({length: 8}, () => 0));
-    const getMoves = ([x, y]) => [
-        [x - 2, y - 1],
-        [x - 2, y + 1],
-        [x - 1, y - 2],
-        [x - 1, y + 2],
-        [x + 1, y - 2],
-        [x + 1, y + 2],
-        [x + 2, y - 1],
-        [x + 2, y + 1],
+const sameArr = (a,b) => JSON.stringify(a) === JSON.stringify(b);
+const Knight = (start, destination=null) => {
+    let board = [];
+    const moves = [
+        [-2, -1],
+        [-1, -2],
+        [1, -2],
+        [2, -1],
+        [2, 1],
+        [1, 2],
+        [-1, 2],
+        [-2, 1],
     ];
-    const isValidMove = ([x,y]) => x >= 0 && x < 8 && y >= 0 && y < 8 && board[x][y] === 0;
-    const path = []
 
-    function dfs(start, count=1){
-        path.push(start);
-        const [x,y] = start;
-        board[x][y] = count;
-        if(count === 64){
-            return true;
-        }
-        
-        const moves = getMoves(start);
-        for(const move of moves){
-            if(isValidMove(move)){
-                if(dfs(move, count + 1)){
-                    return true;
+    const knightTour = () => {
+        board = Array.from({length: 8}, () => Array.from({length: 8}, () => 0));
+        const path = [];
+
+        function dfsHelper(start, count=1){
+            path.push(start);
+            const [x,y] = start;
+            board[x][y] = count;
+            if(count === 64){
+                return true;
+            }
+            
+            const validMoves = moves.map(([dx,dy]) => [x+dx, y+dy]).filter(([x, y]) => x >= 0 && x < 8 && y >= 0 && y < 8 && board[x][y] === 0);
+            validMoves.sort((a,b) => {
+                const aMoves = moves.filter(([dx, dy]) => {
+                    const [x,y] = a;
+                    const nx = x + dx;
+                    const ny = y + dy;
+                    return nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && board[nx][ny] === 0;
+                }).length;
+                const bMoves = moves.filter(([dx, dy]) => {
+                    const [x,y] = b;
+                    const nx = x + dx;
+                    const ny = y + dy;
+                    return nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && board[nx][ny] === 0;
+                }).length;
+
+                return aMoves - bMoves;
+            });
+
+            for(const move of validMoves){
+                if(dfsHelper(move, count+1)){
+                    return path;
                 }
             }
+
+            board[x][y] = 0;
+            path.pop();
+            return false;
         }
-        path.pop();
-        board[x][y] = 0;
-        return false;
-    }
-    dfs(start);
-    return path;
+        return dfsHelper(start);
+    };
+
+    
+
+    return {knightTour, shortestPath};
 };
 
 const createHTMLBoard = size => {
@@ -50,80 +73,126 @@ const createHTMLBoard = size => {
     }
 };
 
-const knightMoves = start => {
-    const path = knightTour(start);
-    const [...squares] = document.querySelectorAll('.board > div');
-    const sameArr = (a,b) => JSON.stringify(a) === JSON.stringify(b);
-    const findSquare = arr => squares.find(square => sameArr(square.dataset.cords.split(',').map(el => +el), arr));
+const createNewLine = (start, end) => {
+    const board = document.querySelector('.board');
+    const line = document.createElement('div');
+    line.classList.add('line');
+    const startX = start.offsetLeft + (start.offsetWidth / 2);
+    const startY = start.offsetTop + (start.offsetHeight / 2);
+    const endX = end.offsetLeft + (end.offsetWidth / 2);
+    const endY = end.offsetTop + (end.offsetHeight / 2);
+    const length = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+    const angle = Math.atan2(endY - startY, endX - startX) * 180 / Math.PI;
 
-    let prevSquare = findSquare(start);
-    animate();
-    function animate(length = 1){
-        if(length === path.length) return;
-        
-        const square = findSquare(path[length]);
-        createNewLine(prevSquare, square);
-        prevSquare = square;
-
-        setTimeout(() => animate(++length),500);
-    }
-
-    function createNewLine(start, end){
-        const board = document.querySelector('.board');
-        const line = document.createElement('div');
-        line.classList.add('line');
-        const startX = start.offsetLeft + (start.offsetWidth / 2);
-        const startY = start.offsetTop + (start.offsetHeight / 2);
-        const endX = end.offsetLeft + (end.offsetWidth / 2);
-        const endY = end.offsetTop + (end.offsetHeight / 2);
-        const length = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
-        const angle = Math.atan2(endY - startY, endX - startX) * 180 / Math.PI;
-
-        line.style.width = length + 'px';
-        line.style.transform = `rotate(${angle}deg)`;
-        line.style.top = startY + 'px';
-        line.style.left = startX + 'px';
-        board.appendChild(line);
-    }
+    line.style.width = length + 'px';
+    line.style.transform = `rotate(${angle}deg)`;
+    line.style.top = startY + 'px';
+    line.style.left = startX + 'px';
+    board.appendChild(line);
 }
 
 const gameFlow = () => {
-    const squares = document.querySelectorAll('.board > div');
+    const board = document.querySelector('.board');
     const buttons = document.querySelectorAll('.container > div > button');
-    let counter = 0;
+    let squares = document.querySelectorAll('.board > div');
+    let firstClick = true;
     let start = null; 
     let destination = null;
+    let knightTask = 'tour';
 
     buttons.forEach(button => button.addEventListener('click', chooseAlg));
+    squares.forEach(square => square.addEventListener('click', squareChosen));
 
     function chooseAlg(e){
-        if(e.target.dataset.id === '1'){
+        if(e.target.name === 'tour'){
             buttons[0].classList.add('active');
             buttons[1].classList.remove('active');
-        }else{
+            reset();
+            knightTask = 'tour';
+        }
+        else if(e.target.name === 'path'){
             buttons[1].classList.add('active');
             buttons[0].classList.remove('active');
+            reset();
+            knightTask = 'path';
+        }
+        else{
+            reset();
         }
     }
 
-    squares.forEach(square => square.addEventListener('click', squareChosen));
-
     function squareChosen(e){
-        if(counter === 2) return;
         const choice = e.target.dataset.cords.split(',').map(el => +el);
-        counter === 0 ? start = choice : destination = choice;
-
-        if(start && destination){
-            knightMoves(start, destination);
-
-            // reset
-            /*counter = 0;
-            start = null;
-            destination = null;
-            startSquareText.textContent = `Start: `;
-            destinationSquareText.textContent = `Destination: `;*/
+        if(knightTask === 'tour'){
+            e.currentTarget.style.backgroundColor = 'green';
+            squares.forEach(square => square.removeEventListener('click', squareChosen));
+            buttons.forEach(button => button.removeEventListener('click', chooseAlg));
+            knightMoves(choice, destination);
+        }else{
+            if(firstClick){
+                start = choice;
+                e.currentTarget.style.backgroundColor = 'green';
+            }else{
+                destination = choice;
+                e.currentTarget.style.backgroundColor = 'orange';
+            }
+            if(start && destination){
+                squares.forEach(square => square.removeEventListener('click', squareChosen));
+                buttons.forEach(button => button.removeEventListener('click', chooseAlg));
+                knightMoves(start, destination);
+            }
         }
-        counter++;
+        firstClick = false;
+    }
+
+    function knightMoves(start, destination){
+        const findSquare = arr => [...squares].find(square => sameArr(square.dataset.cords.split(',').map(el => +el), arr));
+        const knight1 = Knight(start, destination);
+        let path = [];
+        const startSquare = findSquare(start);
+    
+        destination ? path = knight1.shortestPath() : path = knight1.knightTour();
+    
+        let prevSquare = startSquare;
+        const activateBtns = async () => {
+            const result = await animate();
+
+            buttons.forEach(button => button.addEventListener('click', chooseAlg));
+        };
+        activateBtns();
+
+        function animate(){
+            return new Promise(resolve => {
+                function animateHelper(callback, length = 1){
+                    if(length === path.length){
+                        callback(); // call the callback to signal the animation is complete
+                        return true;
+                    }
+            
+                    const square = findSquare(path[length]);
+                    if(destination){
+                        if(path[length+1]) square.style.backgroundColor = 'pink';
+                    }
+                    else square.style.backgroundColor = 'pink';
+                    
+                    createNewLine(prevSquare, square);
+                    prevSquare = square;
+            
+                    setTimeout(() => animateHelper(callback, ++length),500);
+                }
+                animateHelper(() => resolve('Animation complete'));
+            });
+        }
+    }
+
+    function reset(){
+        firstClick = true;
+        start = null; 
+        destination = null;
+        board.innerHTML = '';
+        createHTMLBoard(64);
+        squares = document.querySelectorAll('.board > div');
+        squares.forEach(square => square.addEventListener('click', squareChosen));
     }
 };
 
